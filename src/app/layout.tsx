9e780c220da5bc4a4cbc39a975afc3d635901bb0
imsidkg <siddharth8271skg2@gmail.com>
2024-12-02 20:57:24 +0530

    added clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Manrope, DM_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider"
 
-
+const manrope = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Loume",
@@ -15,12 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
-      <body
-       
-      >
-        {children}
-      </body>
+      <body className={`${manrope.className} bg-[#171717]`}> 
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          </body>
     </html>
+    </ClerkProvider>
   );
 }
