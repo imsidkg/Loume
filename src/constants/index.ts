f5d3ccbd5c85ef80b910a6cbae14d78f1d23da72
imsidkg <siddharth8271skg2@gmail.com>
2024-12-12 02:12:50 +0530

    icons added

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const MENU_ITEMS =  (
+    workspaceId : string
+) : {title : string  , href: string , icon:React.ReactNode}[] => [
+    {
+        title : 'Home',
+        href : `/dashboard/${workspaceId}/home`,
+        icon : 
+    }
+]
