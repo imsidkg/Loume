649882a2f43c9e6a77603a85f1e55e4374a37a64
imsidkg <siddharth8271skg2@gmail.com>
2024-12-29 01:17:20 +0530

    random push

--- src/components/global/folders/folder-info.tsx
+++ src/components/global/folders/folder-info.tsx
@@ -1,13 +1,25 @@
+import { getFolderInfo } from '@/actions/workspace'
+import { useQueryData } from '@/hooks/useQueryData'
+import { FolderProps } from '@/types'
 import React from 'react'
+import { FoldersProps } from '.'
 
 type Props = {
     folderId: string
 }
 
+
 const FolderInfo = ({folderId}: Props) => {
-    
+
+    const {data} = useQueryData(['folder-info'] , () => getFolderInfo(folderId))
+    const {data : folder} = data as FolderProps
+
+
+
   return (
-    <div>FolderInfo</div>
+      <div className="flex items-center">
+      <h2 className="text[#BdBdBd] text-2xl">{folder.name}</h2>
+    </div>
   )
 }
 
---
+++ src/types/index.d.ts
@@ -0,0 +1,9 @@
+export type FolderProps  = {
+    status : number ,
+    data : {
+        name : string ,
+        _count : {
+            videos : number
+        }
+    }
+}
