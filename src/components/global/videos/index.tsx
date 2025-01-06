b9cd061456b610114e46139705fe0e9b51c9c65a
imsidkg <siddharth8271skg2@gmail.com>
2025-01-06 20:29:57 +0530

    vidoe

--- src/components/global/videos/index.tsx
+++ src/components/global/videos/index.tsx
@@ -2,6 +2,9 @@ import { getAllUserVideos } from '@/actions/workspace'
 import { useQueryData } from '@/hooks/useQueryData'
 import { VideosProps } from '@/types'
 import React from 'react'
+import VideoCard from './video-card'
+import VideoRecorderDuotone from '@/components/icons/video-recorder-duotone'
+import { cn } from '@/lib/utils'
 
 type Props = {
   folderId : string
@@ -15,7 +18,33 @@ const Videos = ({folderId ,videoKey , workspaceId}: Props) => {
   const {status : videosStatus , data : videos} = (videoData?? {status : 404 , data : []}) as VideosProps
 
   return (
-    <div>Videos</div>
+    <div className="flex flex-col gap-4 mt-4">
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        <VideoRecorderDuotone />
+        <h2 className="text-[#BdBdBd] text-xl">Videos</h2>
+      </div>
+    </div>
+    <section
+      className={cn(
+        videosStatus !== 200
+          ? 'p-5'
+          : 'grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'
+      )}
+    >
+      {videosStatus === 200 ? (
+        videos.map((video) => (
+          <VideoCard
+            key={video.id}
+            workspaceId={workspaceId}
+            {...video}
+          />
+        ))
+      ) : (
+        <p className="text-[#BDBDBD]"> No videos in workspace</p>
+      )}
+    </section>
+  </div>
   )
 }
 
