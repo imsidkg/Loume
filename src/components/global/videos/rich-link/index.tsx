392288a77132b44a0867977ed84e968e412bf12d
imsidkg <siddharth8271skg2@gmail.com>
2025-01-06 12:06:26 +0530

    rich link added

--- src/components/global/videos/rich-link/index.tsx
+++ src/components/global/videos/rich-link/index.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { toast } from 'sonner'
 
 type Props = {
     title : string,
@@ -8,8 +10,46 @@ type Props = {
 }
 
 const RichLink = ({title , id, source , description}: Props) => {
+
+    const copyRichText = () => {
+        const orignalTitle = title;
+        const thumbnail = `<a style="display: flex; flex-direction: column; gap: 10px" href="${process.env.NEXT_PUBLIC_HOST_URL}/preview/${id}">
+    <h3 style="text-decoration: none; color: black; margin: 0;">${orignalTitle}</h3>
+    <p style="text-decoration: none; color: black; margin: 0;">${description}</p>
+    <video
+        width="320"
+        style="display: block"
+        >
+            <source
+                type="video/webm"
+                src="${process.env.NEXT_PUBLIC_CLOUD_FRONT_STREAM_URL}/${source}"
+            />
+        </video>
+    </a>`
+
+    const thumbnailBlob = new Blob([thumbnail] , {type : 'text/html'})
+    const blobTitle = new Blob([orignalTitle] , {type : 'text/html'})
+    const data = [
+        new ClipboardItem({
+            ['text/plain'] :blobTitle,
+            ['text/html' ] :  thumbnailBlob
+        })
+    ]
+
+
+    navigator.clipboard.write(data).then (() => {
+        toast('Embedded Link Copied' , {
+            description  : 'Successfully copied embedded link',
+        })
+    })
+    }
   return (
-    <div>RichLink</div>
+    <Button
+    onClick={copyRichText}
+    className="rounded-full"
+  >
+    Get Embedded Code
+  </Button>
   )
 }
 
