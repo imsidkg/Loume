3a0da4c7a0b72df16e36cb28720627e6152ae56e
imsidkg <siddharth8271skg2@gmail.com>
2025-01-14 23:53:36 +0530

    create CommentForm ui

--- src/components/forms/comment-form/index.tsx
+++ src/components/forms/comment-form/index.tsx
@@ -1,19 +1,45 @@
-import { useVideoComment } from '@/hooks/useVideoComment'
-import React from 'react'
+import FormGenerator from "@/components/global/form-generator";
+import Loader from "@/components/global/loader";
+import { Button } from "@/components/ui/button";
+import { useVideoComment } from "@/hooks/useVideoComment";
+import { Send } from "lucide-react";
+import React from "react";
 
 type Props = {
-    videoId: string
-    commentId?: string
-    author: string
-    close?: () => void
-}
+  videoId: string;
+  commentId?: string;
+  author: string;
+  close?: () => void;
+};
 
-const CommenForm = ({author, videoId, close, commentId}: Props) => {
-    const {register , errors, isPending , onFormSubmit} = useVideoComment(videoId , commentId)
+const CommenForm = ({ author, videoId, close, commentId }: Props) => {
+  const { register, errors, isPending, onFormSubmit } = useVideoComment(
+    videoId,
+    commentId
+  );
 
-  return (
-    <div>CommenForm</div>
-  )
-}
+  return <form className="relative w-full" onSubmit={onFormSubmit}>
+     <FormGenerator
+        register={register}
+        errors={errors}
+        placeholder={`Respond to ${author}...`}
+        name="comment"
+        inputType="input"
+        lines={8}
+        type="text"
+      />
+      <Button
+        className="p-0 bg-transparent absolute top-[1px] right-3 hover:bg-transparent "
+        type="submit"
+      >
+        <Loader state={isPending}>
+          <Send
+            className="text-white/50 cursor-pointer hover:text-white/80"
+            size={18}
+          />
+        </Loader>
+      </Button>
+  </form>;
+};
 
-export default CommenForm
+export default CommenForm;
